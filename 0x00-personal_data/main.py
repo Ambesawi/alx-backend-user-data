04fe91885ee1de6010d6970d254322577f036668
Ambesawi <ambesawi77@gmail.com>
2023-11-02 19:39:12 +0000

    main.sql

--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -3,10 +3,8 @@
 Main file
 """
 
-filter_datum = __import__('filtered_logger').filter_datum
+hash_password = __import__('encrypt_password').hash_password
 
-fields = ["password", "date_of_birth"]
-messages = ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;", "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"]
-
-for message in messages:
-    print(filter_datum(fields, 'xxx', message, ';'))
+password = "MyAmazingPassw0rd"
+print(hash_password(password))
+print(hash_password(password))
