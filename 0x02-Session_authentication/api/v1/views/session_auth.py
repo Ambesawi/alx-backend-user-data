1e4c0198b4e92951dcd84ff8fcff2f823c467840
Ambesawi <ambesawi77@gmail.com>
2023-11-09 20:25:01 +0000

    session_auth.py

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+""" Module of Users views
+"""
+import os
+from flask import jsonify, request
+from api.v1.views import app_views
+from models.user import User
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def auth_session():
+    """
+    Handle user login
+    Return:
+        dictionary representation of user if found else error message
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email is None or email == '':
+        return jsonify({"error": "email missing"}), 400
+    if password is None or password == '':
+        return jsonify({"error": "password missing"}), 400
+    users = User.search({"email": email})
+    if not users or users == []:
+        return jsonify({"error": "no user found for this email"}), 404
+    for user in users:
+        if user.is_valid_password(password):
+            from api.v1.app import auth
+            session_id = auth.create_session(user.id)
+            resp = jsonify(user.to_json())
+            session_name = os.getenv('SESSION_NAME')
+            resp.set_cookie(session_name, session_id)
+            return resp
+    return jsonify({"error": "wrong password"}), 401
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def handle_logout():
+    """
+    Handle user logout
+    """
+    from api.v1.app import auth
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+    abort(404)
