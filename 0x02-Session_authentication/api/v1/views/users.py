54becb887880ee1d1bc45b0515a8ee307f12941b
Ambesawi <ambesawi77@gmail.com>
2023-11-09 21:17:50 +0000

    users.py

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -27,8 +27,15 @@ def view_one_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
+    if user_id == "me":
+        if request.current_user is None:
+            abort(404)
+        user = request.current_user
+        return jsonify(user.to_json())
     user = User.get(user_id)
     if user is None:
+        abort(404)
+    if request.current_user is None:
         abort(404)
     return jsonify(user.to_json())
 
