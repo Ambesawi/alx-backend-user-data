bd2f3f31284f3572030021af9f01e316334ea66a
Ambesawi <ambesawi77@gmail.com>
2023-11-16 20:11:07 +0000

    app.py

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -10,8 +10,5 @@ auth = Auth()
 
 auth.register_user(email, password)
 
-print(auth.valid_login(email, password))
-
-print(auth.valid_login(email, "WrongPwd"))
-
-print(auth.valid_login("unknown@email", password))
+print(auth.create_session(email))
+print(auth.create_session("unknown@email.com"))
