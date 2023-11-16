0c3880af8105ecf846e45f50f892464fcb02e139
Ambesawi <ambesawi77@gmail.com>
2023-11-16 20:05:22 +0000

    auth.py

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -2,6 +2,21 @@
 """
 Main file
 """
-from auth import _hash_password
+from auth import Auth
 
-print(_hash_password("Hello Holberton"))
+email = 'me@me.com'
+password = 'mySecuredPwd'
+
+auth = Auth()
+
+try:
+    user = auth.register_user(email, password)
+    print("successfully created a new user!")
+except ValueError as err:
+    print("could not create a new user: {}".format(err))
+
+try:
+    user = auth.register_user(email, password)
+    print("successfully created a new user!")
+except ValueError as err:
+    print("could not create a new user: {}".format(err))
