abceb89ac8c1c2503e439f045bc42deb67f28929
Ambesawi <ambesawi77@gmail.com>
2023-11-16 19:57:14 +0000

    main.py

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -2,9 +2,14 @@
 """
 Main file
 """
+
+from db import DB
 from user import User
 
-print(User.__tablename__)
+my_db = DB()
 
-for column in User.__table__.columns:
-    print("{}: {}".format(column, column.type))
+user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
+print(user_1.id)
+
+user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
+print(user_2.id)
