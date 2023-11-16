eb99a5b37671a8c99f085ae6051f8b6c4bad10e8
Ambesawi <ambesawi77@gmail.com>
2023-11-16 20:02:36 +0000

    main.py

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -2,23 +2,6 @@
 """
 Main file
 """
-from db import DB
-from user import User
+from auth import _hash_password
 
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
-
-
-my_db = DB()
-
-email = 'test@test.com'
-hashed_password = "hashedPwd"
-
-user = my_db.add_user(email, hashed_password)
-print(user.id)
-
-try:
-    my_db.update_user(user.id, hashed_password='NewPwd')
-    print("Password updated")
-except ValueError:
-    print("Error")
+print(_hash_password("Hello Holberton"))
